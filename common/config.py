892c752e80dc5814d95d1073875f492893e0bc11
Nick McIntyre <nam219@lehigh.edu>
2020-03-10 12:20:20 -0400

    First commit. Adds all files from original code that is used.

---
+++ common/config.py
@@ -0,0 +1,12 @@
+h264_folder='h264_videos/'
+mp4_folder='mp4_videos/'
+log_dir='logs/'
+log_file='files_not_sent.txt'
+countfile='common/countfile.txt'
+logwrite_th=15 #seconds
+connect_th=15
+motion_threshold=25
+initial_sleep=3
+send_time_threshold=-1 #no threshold
+location='Asia/Kolkata'
+usbcam=False #if false, CSI camera is assumed
