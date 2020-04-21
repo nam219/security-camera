6b3f5b75f382297b3d3ffc23b25b06275ef076a7
nam219 <nickmcintyre844@gmail.com>
2020-04-21 16:42:25 -0400

    Update start_jetson.py

--- rpi/start_jetson.py
+++ rpi/start_jetson.py
@@ -3,12 +3,8 @@ import sys, os
 #point was to call this program and then continue on in motion detector, even though this does so little
 if __name__ == '__main__':
     #os.system('sshfs nick@192.168.1.135:/home/nick/camera_server/security-camera/nano ./jetson-mount')
-<<<<<<< HEAD
     os.system("ssh nick@192.168.1.135 'cd /home/nick/camera_server/security-camera/nano; python3 main.py {0} {1}'".format(sys.argv[1], sys.argv[len(sys.argv)-1]))
     #os.system("python3 main.py {0} {1}".format(sys.argv[1], sys.argv[len(sys.argv)-1]))
-=======
     #os.system("ssh nick@jetson: 'cd /home/nick/camera_server/security-camera/nano; python3 main.py {0} {1}'".format(sys.argv[1], sys.argv[len(sys.argv)-1]))
-    os.system("ssh nick@192.168.1.135 'cd /home/nick/camera_server/security-camera/nano; python3 main.py {0} {1}'".format(sys.argv[1], sys.argv[len(sys.argv)-1]))
-
+    #os.system("ssh nick@192.168.1.135 'cd /home/nick/camera_server/security-camera/nano; python3 main.py {0} {1}'".format(sys.argv[1], sys.argv[len(sys.argv)-1]))
     #os.system("python3 main.py {0} {1}".format(sys.argv[1], sys.argv[len(sys.argv)-1]))
->>>>>>> 3f1ef2ada5e8d37a1830693407a4cb79718c1376
